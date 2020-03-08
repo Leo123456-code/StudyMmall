49b2abcf60b332188a952f9d6bb4e1a6398a7b7d
Administrator <1437665365@qq.com>
2020-03-09 03:59:52 +0800

    first commit init project

---
+++ src/main/java/com/imooc/demo/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.imooc.demo.dao;
+
+import com.imooc.demo.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
